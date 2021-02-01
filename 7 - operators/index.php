41077d25acc2acf061b28dfe1a9ce33a5ee611b9
Fiki Alamsyah <fiki@pop-os.localdomain>
2021-02-01 23:18:50 +0700

    -add 7 - operator dir

---
+++ 7 - operators/index.php
@@ -0,0 +1,48 @@
+<?php
+
+// operator
+// 1. aritmatika "+ - / * % **"
+echo 10 + 2;
+echo "<br/>";
+echo 10 - 2;
+echo "<br/>";
+echo 10 * 2;
+echo "<br/>";
+echo 10 / 2;
+echo "<br/>";
+echo 10 % 2;
+echo "<br/>";
+echo 10 ** 2;
+echo "<br/>";
+
+
+// 2. penugasan "="
+$x = 10;
+$y = 5;
+
+echo $x;
+echo "<br/>";
+echo $y;
+echo "<br/>";
+
+
+// 3. perbandingan "==, ===, !=, <>, !==, >, <, <=,>=,<=>"
+echo 5 == 5;
+echo "<br/>";
+echo 5 != 6;
+echo "<br/>";
+echo 10 >= 7;
+echo "<br/>";
+echo 5 <= 10;
+echo "<br/>";
+
+
+// 4. Logika "and, or, xor, &&, ||, !"
+echo 5 == 5 and 5 < 6;
+echo "<br/>";
+echo 4 < 6 or 10 <= 7;
+echo "<br/>";
+
+
+
+?>
