59e60d09fe4337dabce21025fbf5d2e865a85234
Fiki Alamsyah <fiki@pop-os.localdomain>
2021-02-01 22:03:42 +0700

    add 3 - echo & print dir

---
+++ 3 - echo & print/index.php
@@ -0,0 +1,19 @@
+<?php
+
+// echo  : tidak memiliki nilai pengembalian
+// print : memiliki nilai pengembalian 1
+// cara 1 tanpa ()
+echo "<p>Belajar php itu asyik</p>";
+print "<p>Belajar php itu asyik</p>";
+
+// cara 2 dengan ()
+echo("<p>Belajar php itu asyik</p>");
+print("<p>Belajar php itu asyik</p>");
+
+// dengan menggunakan variable
+$txt = "php";
+echo "<p>$txt is awesome</p>";
+print "<p>$txt is awesome</p>";
+
+    
+?>
