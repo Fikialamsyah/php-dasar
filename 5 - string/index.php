c1cf65639921191220436481eec33428035cbbf1
Fiki Alamsyah <fiki@pop-os.localdomain>
2021-02-01 23:11:30 +0700

    add 5 - string/ dir

---
+++ 5 - string/index.php
@@ -0,0 +1,27 @@
+<?php
+
+// method - method string
+// strlen() : mengembalikan panjang string
+echo strlen("Fiki Alamsyah");
+
+echo "<br/>";
+
+// str_word_count() : menghitung jumlah kata dalam sebuah string
+echo str_word_count("Fiki Alamsyah");
+
+echo "<br/>";
+
+// strrev() : membalikkan string
+echo strrev("Fiki alamsyah");
+
+echo "<br/>";
+
+// strpos() : mencari teks tertentu
+echo strpos("Fiki alamsyah", "alamsyah");
+
+echo "<br/>";
+
+// str_replace() : mengganti beberapa karakter dalam string
+echo str_replace("Fiki", "Fika", "Fiki Alamsyah");
+
+?>
