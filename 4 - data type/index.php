5e2f58408aa60bb848e6944520b83bc4783ae09b
Fiki Alamsyah <fiki@pop-os.localdomain>
2021-02-01 22:21:44 +0700

    add 4 - data type dir

---
+++ 4 - data type/index.php
@@ -0,0 +1,40 @@
+<?php 
+
+// data type : string, integer, float, boolean, array, object, null, resource
+// 1. string
+$nama = "Fiki";
+var_dump($nama);
+
+echo "</br>";
+
+// 2. integer
+$x = 10;
+var_dump($x);
+
+echo "</br>";
+
+// 3. float
+$y = 12.5;
+var_dump($y);
+
+echo "</br>";
+
+// boolean
+$isHujan = true;
+var_dump($isHujan);
+
+echo "</br>";
+
+// array
+$buah = array("Apel", "Jeruk", "Mangga");
+var_dump($buah);
+
+echo "</br>";
+
+
+// null
+$number = null;
+var_dump($number);
+
+
+?>
