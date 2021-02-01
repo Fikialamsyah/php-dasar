fb57fbd7b4d4a4bf51aa2fbec8cb0ffa5cf6e195
Fiki Alamsyah <fiki@pop-os.localdomain>
2021-02-01 23:12:35 +0700

    add 6 - constanta dir

---
+++ 6 - constanta/index.php
@@ -0,0 +1,21 @@
+<?php
+
+// membuat constanta 
+define("nama", "Fiki Alamsyah");
+
+// panggil const
+echo nama;
+
+echo "<br/>";
+
+// array const
+define("buah", [
+    "Apel",
+    "Mangga",
+    "Jeruk"
+]);
+
+// panggil array const
+echo buah[0];
+
+?>
